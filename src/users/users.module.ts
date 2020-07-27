96b0810a10b745b63f93815d3931b8152db0476b
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-07-27 12:46:38 +0200

    Basic structure

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './ui/users.controller';
+import { UsersService } from './app/users.service';
 
-@Module({})
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService]
+})
 export class UsersModule {}
