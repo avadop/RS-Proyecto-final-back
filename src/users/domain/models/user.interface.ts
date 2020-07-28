fa804f94bfae0873654d5a620e6f1192be267ba0
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-07-28 12:44:47 +0200

    Model updated

--- src/users/domain/models/user.interface.ts
+++ src/users/domain/models/user.interface.ts
@@ -1,7 +1,19 @@
 import { Document } from 'mongoose';
+import { Address } from '../interfaces/address.interface';
+import { Insurance } from '../interfaces/insurance.interface';
 
 export interface User extends Document{
+  nhc?: string; 
+  insuranceList?: Insurance[];
+
+  membershipNumber?: string;
+  professionalType?: string;
+  
   name: string;
   firstSurname: string; 
   secondSurname: string;
+  gender?: string;
+  birthDate?: string;
+  idDocument?: string;
+  address?: Address;
 };
